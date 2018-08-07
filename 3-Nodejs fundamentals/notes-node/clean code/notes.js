859829389c98d78eb7b1062bd60ebc1c18be6cca
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-08-07 14:40:29 +0530

    updated note

--- 3-Nodejs fundamentals/notes-node/clean code/notes.js
+++ 3-Nodejs fundamentals/notes-node/clean code/notes.js
@@ -1,3 +1,25 @@
 console.log("starting notes.js");
 
-module.exports.age = 25;
+var addNote = (title, body) => {
+    console.log('adding Note', title, body);
+}
+
+var getAll = () => {
+    console.log("getting all notes");
+}
+
+var getNote = (title) => {
+    console.log('getting note', title);
+}
+
+var removeNote = (title) => {
+    console.log('removing note', title);
+}
+
+module.exports = {
+    //addNote: addNote // if we have same property and value we can leave it withn single name bcoz of es6 syntax
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+}
