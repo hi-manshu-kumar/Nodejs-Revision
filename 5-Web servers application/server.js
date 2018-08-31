704324e15775d973bfb8a7372d6fc978d5ce3a25
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-08-31 21:27:54 +0530

    trying to connect to heroku

--- 5-Web servers application/server.js
+++ 5-Web servers application/server.js
@@ -1,24 +1,53 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}` ; 
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err){
+            console.log('Unable to append to server.log')
+        }
+    });
+
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+
+// app.use((req, res, next) => {
+//     res.render('maintainence.hbs');
+// });
+
 
 app.get('/', (req, res) => {
     res.render( 'home.hbs',{
         pageTitle: 'Home',
-        message: 'Welcome to our page',
-        currentYear: new Date().getFullYear()
+        message: 'Welcome to our page'
     });
-});
+});   
 
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle:'About page',
-        currentYear: new Date().getFullYear()
+        pageTitle:'About page'
     });
 }); 
 
@@ -27,6 +56,8 @@ app.get('/bad', (req, res) => {
           errorMessage: 'errr'
       });
 });
-app.listen(3000, ()=> {
-    console.log('Server is running at port 3000');
+
+
+app.listen(port , ()=> {
+    console.log(`Server is running at port ${port}`);
 });
