1d50d56d5023e6a55812b8649c51190ed9935823
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-08-31 12:21:50 +0530

    callbacks

--- 4-Asynch nodejs (weather app)/weather app/app.js
+++ 4-Asynch nodejs (weather app)/weather app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
     .options({
@@ -14,16 +14,14 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv);
+// console.log(argv);
 
-const encodeAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
+    }else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
 
-request({
-    url: ` https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
-    json: true
-},(error, response, body) => {
-    console.log(`Address : ${body.results[0].formatted_address}`);
-    console.log(`Latitude : ${body.results[0].geometry.location.lat}  `);
-    console.log(`Latitude : ${body.results[0].geometry.location.lng}`)
-}); 
 
