b57d3980b4c249518949df4a1cc2644e0a6dfe4a
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-10-16 13:25:30 +0530

    wroking with mongoose

--- 6-Mongodb/node-todo-api/server/server.js
+++ 6-Mongodb/node-todo-api/server/server.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo',{ 
+var Todo = mongoose.model('Todo',{ // model is the method that we use to create new model , it takwes 2 argument,
+    //1st one contaitns the string and 2nd one is the object which will define the various properties of the model
     text: {
         type:String
     },
@@ -25,14 +26,25 @@ var Todo = mongoose.model('Todo',{
 //     console.log("unable to save todo")
 // });
 
-var otherTodo = new Todo({
-    text: 'do something',
-    completed: false,
-    completedAt : 123 
-})
+// var otherTodo = new Todo({
+//     text: 'do something',
+//     completed: false,
+//     completedAt : 123 
+// })
 
-otherTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log("unable to save todo")
+// otherTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log("unable to save todo")
+// });
+
+
+var newToDo2 = new Todo({
+    text: "cook lunch",
+    completed: "false",
+    completedat: 123
 });
+
+newToDo2.save().then((res) => {
+    console.log("new todo added", res);
+}, (err) => console.log("error is", err));
