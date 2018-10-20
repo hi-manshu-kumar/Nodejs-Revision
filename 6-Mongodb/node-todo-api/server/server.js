568df45667f2e68dd927f3677ea49b7a4747968d
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-10-20 14:20:14 +0530

    jwt and generating headers for user

--- 6-Mongodb/node-todo-api/server/server.js
+++ 6-Mongodb/node-todo-api/server/server.js
@@ -96,15 +96,18 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 // POST /users
-app.post("/users", (req, res) => {
-    var body = _.pick(req.body, ['email','password']); 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-
-    user.save().then((user) => {
-        res.send(user);
-    }).catch((e) => res.status(404).send(e))
+  
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
-
 app.listen(port, ()=> {
     console.log(`server running on ${port}....`);
 });
