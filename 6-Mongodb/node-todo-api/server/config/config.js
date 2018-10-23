117337fec50f4e3782010367b39b59c0c87a96ef
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-10-24 03:46:13 +0530

    improving app configuration

--- 6-Mongodb/node-todo-api/server/config/config.js
+++ 6-Mongodb/node-todo-api/server/config/config.js
@@ -1,13 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ********* ', env);
 
-if(env === 'development'){
-    //locally for development
-    process.env.PORT = 3000; //since the port will be added by heroku if its on production mode
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    // custom database url
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    // console.log(config);
+    var envConfig = config[env];
 
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]; 
+    });
 }
+// if(env === 'development'){
+//     //locally for development
+//     process.env.PORT = 3000; //since the port will be added by heroku if its on production mode
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if(env === 'test'){
+//     // custom database url
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
